212b81cf268024e76427fb617df4e7931cad6305
ilkin Nazarli <ilkinnazarli@gmail.com>
2020-10-28 05:01:00 +0400

    1 add navbar 2 add home slider

---
+++ Assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".home-carousel").owlCarousel({
+        items: 1,
+        singleItem: true,
+        loop:true,
+        margin:0,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        transitionStyle:"fade"
+    });
+  });
