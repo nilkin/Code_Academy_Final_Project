28917e849a19405282c91884283e27d8d7ca6b7c
ilkin Nazarli <ilkinnazarli@gmail.com>
2020-10-29 05:41:08 +0400

    1 add animate 2 done slider 3 add dropdown

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,11 +1,46 @@
-$(document).ready(function(){
-    $(".home-carousel").owlCarousel({
-        items: 1,
-        singleItem: true,
-        loop:true,
-        margin:0,
+$(document).ready(function () {
+    // dropdown
+    $('.nav-item').mouseenter(function(){
+        $(this).find('.dropdown-menu').addClass("show");
+      });
+      
+      $('.nav-item').mouseleave(function(){
+        $(this).find('.dropdown-menu').removeClass("show");
+      });
+    // main-slider
+    var $timer = 6000;
+    $('.home-carousel').slick({
         autoplay: true,
-        autoplayTimeout: 10000,
-        transitionStyle:"fade"
+        speed: 800,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplaySpeed: $timer,
+        dots: true,
+        fade: true,
+        cssEase: 'linear',
+        arrows: false,
+        responsive: [{
+            breakpoint: 767,
+            settings: {
+                dots: false,
+                arrows: false
+            }
+        }]
+
+    }).slickAnimation();
+
+    function progressBar() {
+        $(".slick-progress").find("span").removeAttr("style");
+        $(".slick-progress").find("span").removeClass("active");
+        setTimeout(function () {
+            $(".slick-progress").find("span").css("transition-duration", $timer / 1000 + "s").addClass("active");
+        }, 100);
+    }
+
+    progressBar();
+    $('.home-carousel').on("beforeChange", function (e, slick) {
+        progressBar();
     });
-  });
+
+
+});
