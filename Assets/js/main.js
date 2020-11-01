e1b269014f60f9c022af9d8c332e305aee867476
ilkin Nazarli <ilkinnazarli@gmail.com>
2020-11-01 17:50:18 +0400

    1add testimonial section 2 add feature new product section

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,135 +1,245 @@
 $(document).ready(function () {
-    // dropdown
-    $('.nav-item').mouseenter(function(){
-        $(this).find('.dropdown-menu').addClass("show").addClass("animated fadeIn");
-      });
+  // dropdown
+  $('.nav-item').mouseenter(function () {
+    $(this).find('.dropdown-menu').addClass("show").addClass("animated fadeIn");
+  });
 
-      $('.nav-item').mouseleave(function(){
-        $(this).find('.dropdown-menu').removeClass(" animated fadeIn").removeClass("show");
-      });
-    // main-slider
-    var $timer = 6000;
-    $('.home-carousel').slick({
-        autoplay: true,
-        speed: 800,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplaySpeed: $timer,
-        dots: true,
-        fade: true,
-        cssEase: 'linear',
-        arrows: false,
-        responsive: [{
-            breakpoint: 767,
-            settings: {
-                dots: false,
-                arrows: false
-            }
-        }]
-
-    }).slickAnimation();
-
-    function progressBar() {
-        $(".slick-progress").find("span").removeAttr("style");
-        $(".slick-progress").find("span").removeClass("active");
-        setTimeout(function () {
-            $(".slick-progress").find("span").css("transition-duration", $timer / 1000 + "s").addClass("active");
-        }, 100);
-    }
-
-    progressBar();
-    $('.home-carousel').on("beforeChange", function (e, slick) {
-        progressBar();
-    });
-    // blog-section-slider
-    $('.new-blog-carousel').slick({
-        
+  $('.nav-item').mouseleave(function () {
+    $(this).find('.dropdown-menu').removeClass(" animated fadeIn").removeClass("show");
+  });
+  // main-slider
+  var $timer = 6000;
+  $('.home-carousel').slick({
+    autoplay: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: $timer,
+    dots: true,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    responsive: [{
+      breakpoint: 767,
+      settings: {
         dots: false,
-        arrows: true,
-        infinite: false,
+        arrows: false
+      }
+    }]
+
+  }).slickAnimation();
+
+  function progressBar() {
+    $(".slick-progress").find("span").removeAttr("style");
+    $(".slick-progress").find("span").removeClass("active");
+    setTimeout(function () {
+      $(".slick-progress").find("span").css("transition-duration", $timer / 1000 + "s").addClass("active");
+    }, 100);
+  }
+
+  progressBar();
+  $('.home-carousel').on("beforeChange", function (e, slick) {
+    progressBar();
+  });
+  // blog-section-slider
+ // blog-section-slider
+  $('.new-blog-carousel').slick({
+        
+  dots: false,
+  arrows: true,
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow:"<div class='arrows'><button type='button' class='prev slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button></div>",
+  nextArrow:"<div class='arrows'><button type='button' class='next slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button></div>",
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
-        prevArrow: $('.prev'),
-        nextArrow: $('.next'),
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-    });
-
-//product tab
-var closestsElementClass = function (elem, className) {
-  var node = elem;
-  while (node) {
-
-    if (node.classList.contains(className)) {
-      return node;
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
     }
-
-    node = node.parentElement;
-  }
-
-  return null;
-}
-var catalog = document.querySelector('.portfolio-container');
-var catalogNav = document.querySelector('.filter__tabs');
-var catalogItems = document.querySelectorAll('.portfolio__item');
-function removeChildren(item) {
-  while (item.firstChild) {
-    item.removeChild(item.firstChild)
-  }
-}
-function updateChildren(item, children) {
-  removeChildren(item);
-  for (var i = 0; i < children.length; i++) {
-    item.appendChild(children[i]);
-  }
-}
-catalogNav.addEventListener('click', function (e) {
-  var target = e.target;
-  var item = closestsElementClass(target, 'filter__btn');
-  if (item === null || item.classList.contains('is-active')) {
-    return;
-  }
-  e.preventDefault();
-  var filterValue = item.getAttribute('data-filter');
-  var previousActiveBtn = document.querySelector('.filter__btn.is-active');
-  previousActiveBtn.classList.remove('is-active');
-  item.classList.add('is-active');
-
-  if (filterValue === 'all') {
-    updateChildren(catalog, catalogItems);
-    return;
-  }
-  var filteredItems = [];
-  for (var i = 0; i < catalogItems.length; i++) {
-    var currentItem = catalogItems[i];
-    if (currentItem.getAttribute('data-category') === filterValue) {
-      filteredItems.push(currentItem);
-    }
-  }
-  updateChildren(catalog, filteredItems);
+  ]
 });
+  //product tab  
+  $('.product-tab-slider').slick({
+      dots: false,
+      arrows: true,
+      infinite: false,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      prevArrow:"<div class='arrows'><button type='button' class='prev slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button></div>",
+      nextArrow:"<div class='arrows'><button type='button' class='next slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button></div>",
+ 
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,         
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+      $('.product-tab-slider').slick('setPosition');
+    })
+    // feature-slider
+  $('.feature-slider').slick({
+      dots: false,
+      arrows: true,
+      infinite: false,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      prevArrow:"<div class='arrows'><button type='button' class='prev slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button></div>",
+      nextArrow:"<div class='arrows'><button type='button' class='next slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button></div>",
+ 
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,         
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+// new-products
+    $('.new-products').slick({
+      dots: false,
+      arrows: true,
+      infinite: false,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      prevArrow:"<div class='arrows'><button type='button' class='prev slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button></div>",
+      nextArrow:"<div class='arrows'><button type='button' class='next slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button></div>",
+ 
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,         
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  //  testimonial-init
+  const $testimonial = $(".testimonial-init");
 
+  $testimonial.slick({
+    autoplay: false,
+    autoplaySpeed: 10000,
+    dots: true,
+    infinite: false,
+    arrows: false,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: true,
+        },
+      },
+
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: true,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
   });
