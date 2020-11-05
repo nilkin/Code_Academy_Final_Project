9e5efb9ecdf097cae4c9330e7dd1f0b03891286e
ilkin Nazarli <ilkinnazarli@gmail.com>
2020-11-06 02:38:44 +0400

    add category grid

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -348,5 +348,50 @@ $(document).ready(function() {
             item.find((e) => e.classList.contains("active")).classList.remove("active");
         });
     };
+    ////////////////////////////////////////////////////////////////////////////////////
 
+    $(".collapser").on("click", function() {
+        $(this).toggleClass("collapsed open");
+        $(this).next().collapse("toggle");
+        if ($(this).children().hasClass("fa-plus")) {
+            $(this).children().removeClass("fa-plus").addClass("fa-minus");
+        } else {
+            $(this).children().addClass("fa-plus");
+        }
+    });
+
+    // ///////////////////////////////////////////////////////////////////
+    var lowerSlider = document.querySelector('#lower');
+    var upperSlider = document.querySelector('#upper');
+
+    document.querySelector('#two').value = upperSlider.value;
+    document.querySelector('#one').value = lowerSlider.value;
+
+    var lowerVal = parseInt(lowerSlider.value);
+    var upperVal = parseInt(upperSlider.value);
+
+    upperSlider.oninput = function() {
+        lowerVal = parseInt(lowerSlider.value);
+        upperVal = parseInt(upperSlider.value);
+
+        if (upperVal < lowerVal + 4) {
+            lowerSlider.value = upperVal - 4;
+            if (lowerVal == lowerSlider.min) {
+                upperSlider.value = 4;
+            }
+        }
+        document.querySelector('#two').value = this.value
+    };
+
+    lowerSlider.oninput = function() {
+        lowerVal = parseInt(lowerSlider.value);
+        upperVal = parseInt(upperSlider.value);
+        if (lowerVal > upperVal - 4) {
+            upperSlider.value = lowerVal + 4;
+            if (upperVal == upperSlider.max) {
+                lowerSlider.value = parseInt(upperSlider.max) - 4;
+            }
+        }
+        document.querySelector('#one').value = this.value
+    };
 });
