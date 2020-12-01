467ef699066aa36dfc2bc06b5857e0467386206e
ilkin Nazarli <ilkinnazarli@gmail.com>
2020-12-01 17:42:50 +0400

    add breadcrumb CRUD

--- ZoNaN/Areas/Manager/Controllers/BreadcrumbController.cs
+++ ZoNaN/Areas/Manager/Controllers/BreadcrumbController.cs
@@ -1,70 +1,67 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ZoNaN.Data;
-using ZoNaN.Models;
+using ZoNaN.Data.Models;
 using static ZoNaN.Services.FileUploader;
 
 namespace ZoNaN.Areas.Manager.Controllers
 {
     [Area("Manager")]
-    public class BlogController : Controller
+    public class BreadcrumbController : Controller
     {
         private readonly ZonanDbContext _context;
         private readonly IFileManager _fileManager;
-        public BlogController(ZonanDbContext context, IFileManager fileManager)
+        public BreadcrumbController(ZonanDbContext context, IFileManager fileManager)
         {
             _context = context;
             _fileManager = fileManager;
         }
-        // GET: BlogController
-        public async Task<IActionResult> Index()
+        // GET: BreadcrumbController
+        public async Task<ActionResult> Index()
         {
-            return View(await _context.Blogs.ToListAsync());
+            return View(await _context.Breadcrumbs.ToListAsync());
         }
 
-        // GET: BlogController/Create
+        // GET: BreadcrumbController/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: BlogController/Create
+        // POST: BreadcrumbController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Blog blog)
+        public async Task<ActionResult> Create(Breadcrumb breadcrumb)
         {
-            if (blog.Upload == null)
+            if (breadcrumb.Upload == null)
             {
                 ModelState.AddModelError("Upload", "Photo is required");
             }
             else
             {
-                if (blog.Upload.ContentType != "image/jpeg" && blog.Upload.ContentType != "image/png" && blog.Upload.ContentType != "image/gif")
+                if (breadcrumb.Upload.ContentType != "image/jpeg" && breadcrumb.Upload.ContentType != "image/png" && breadcrumb.Upload.ContentType != "image/gif")
                 {
                     ModelState.AddModelError("Upload", "You can only download png, jpg or gif file");
                 }
 
-                if (blog.Upload.Length > 1048576)
+                if (breadcrumb.Upload.Length > 1048576)
                 {
                     ModelState.AddModelError("Upload", "The file size can be a maximum of 1 MB");
                 }
             }
             if (ModelState.IsValid)
             {
-                blog.Photo = _fileManager.Upload(blog.Upload, "wwwroot/uploads");
-                _context.Add(blog);
+                breadcrumb.Photo = _fileManager.Upload(breadcrumb.Upload, "wwwroot/uploads");
+                _context.Add(breadcrumb);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(blog);
+            return View(breadcrumb);
         }
 
-        // GET: BlogController/Edit/5
+        // GET: BreadcrumbController/Edit/5
         public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
@@ -73,35 +70,35 @@ namespace ZoNaN.Areas.Manager.Controllers
                 return NotFound();
             }
 
-            var blog = await _context.Blogs.FindAsync(id);
-            if (blog == null)
+            var breadcrumb = await _context.Breadcrumbs.FindAsync(id);
+            if (breadcrumb == null)
             {
                 return NotFound();
             }
-            return View(blog);
+            return View(breadcrumb);
         }
 
-        // POST: BlogController/Edit/5
+        // POST: BreadcrumbController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, Blog blog)
+        public async Task<ActionResult> Edit(int id, Breadcrumb breadcrumb)
         {
-            if (id != blog.Id)
+            if (id != breadcrumb.Id)
             {
                 return NotFound();
             }
-            if (blog.Upload == null)
+            if (breadcrumb.Upload == null)
             {
                 ModelState.AddModelError("Upload", "Photo is required");
             }
             else
             {
-                if (blog.Upload.ContentType != "image/jpeg" && blog.Upload.ContentType != "image/png"
-                 && blog.Upload.ContentType != "image/gif")
+                if (breadcrumb.Upload.ContentType != "image/jpeg" && breadcrumb.Upload.ContentType != "image/png"
+                 && breadcrumb.Upload.ContentType != "image/gif")
                 {
                     ModelState.AddModelError("Upload", "You can only download png, jpg or gif file");
                 }
-                if (blog.Upload.Length > 1048576)
+                if (breadcrumb.Upload.Length > 1048576)
                 {
                     ModelState.AddModelError("Upload", "The file size can be a maximum of 1 MB");
                 }
@@ -111,14 +108,14 @@ namespace ZoNaN.Areas.Manager.Controllers
                 try
                 {
 
-                    _fileManager.Delete(blog.Photo);
-                    blog.Photo = _fileManager.Upload(blog.Upload);
-                    _context.Update(blog).State = EntityState.Modified;
+                    _fileManager.Delete(breadcrumb.Photo);
+                    breadcrumb.Photo = _fileManager.Upload(breadcrumb.Upload);
+                    _context.Update(breadcrumb).State = EntityState.Modified;
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!BlogsExists(blog.Id))
+                    if (!BreadcrumbExists(breadcrumb.Id))
                     {
                         return NotFound();
                     }
@@ -129,27 +126,32 @@ namespace ZoNaN.Areas.Manager.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(blog);
+            return View(breadcrumb);
         }
 
-        // GET: BlogController/Delete/5
-        public async Task<IActionResult> Delete(int Id)
+        private bool BreadcrumbExists(int Id)
         {
-            var blog = await _context.Blogs.FindAsync(Id);
+            return _context.Breadcrumbs.Any(e => e.Id == Id);
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+            // POST: BreadcrumbController/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+            var breadcrumb = _context.Breadcrumbs.Find(Id);
 
-            if (blog == null) return NotFound();
+            if (breadcrumb == null) return NotFound();
 
-            _fileManager.Delete(blog.Photo);
+            _fileManager.Delete(breadcrumb.Photo);
 
-            _context.Blogs.Remove(blog);
+            _context.Breadcrumbs.Remove(breadcrumb);
 
-            await _context.SaveChangesAsync();
+            _context.SaveChanges();
 
             return RedirectToAction("Index");
-        }
-        private bool BlogsExists(int ıd)
-        {
-            return _context.Blogs.Any(e => e.Id == ıd);
         }
 
     }
